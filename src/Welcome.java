45cdb824954c5931dca1357f6231c30949ed67af
vankai48f1 <vankai48f1@gmail.com>
2024-08-15 01:09:47 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Mai Văn Khai");
+        System.out.println("Welcome to the DHKTPM19B");
+    }
+}
